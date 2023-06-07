a8814dbb16e760c39c408c86078a79d97ca7deda
Günther Tresor <31827816+gunthertresor@users.noreply.github.com>
2023-06-07 10:18:18 +0200

    Update index.js

--- GPSCoordinates/lib/index.js
+++ GPSCoordinates/lib/index.js
@@ -14,8 +14,6 @@ const locationMap = (longitude, latitude) => {
     .setLngLat([longitude, latitude])
     .addTo(map);
 };
-
-
 const geoMap = () => {
    // Get the input address element
   const inputAddress = document.querySelector('.form-control');
